c3350066cf71476a020c3ce2be911c6c2751805f
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-03 23:49:42 +0530

    Update userinfo.py

--- TG_ROBOT/modules/userinfo.py
+++ TG_ROBOT/modules/userinfo.py
@@ -474,8 +474,8 @@ def stats(update, context):
             status
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
-            + f"\n\n[✦ Support](https://t.me/{SUPPORT_CHAT}) | [✦ Updates](https://t.me/Black_Knights_Union)\n\n"
-            + "╘══「 by [Awesome RJ](https://github.com/Awesome-RJ) 」\n",
+            + f"\n\n[✦ Support](https://t.me/{SUPPORT_CHAT}) | [✦ Updates](https://t.me/Rem_updates)\n\n"
+            + "╘══「 by [ANKUSH](https://github.com/T-O-B-I-I) 」\n",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
         )
