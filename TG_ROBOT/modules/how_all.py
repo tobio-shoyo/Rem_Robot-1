20f38bd5822cfc03fa2d93440ad76d970131ae7d
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-12 22:38:51 +0530

    Update how_all.py

--- TG_ROBOT/modules/how_all.py
+++ TG_ROBOT/modules/how_all.py
@@ -20,17 +20,79 @@ NUM1 = (
     "25cm",
 )
 
+NUM2 = (
+    "6%",
+    "9%",
+    "15%",
+    "16%",
+    "20%",
+    "22%",
+    "24%",
+    "25%",
+)
+
+NUM3 = (
+    "6",
+    "9",
+    "15",
+    "16",
+    "20",
+    "22",
+    "24",
+    "25",
+)
+
 MEDIA = "https://telegra.ph/file/d55e9149fd684bbe62aa6.mp4"
 
 def cock(update: Update, context: CallbackContext):
     msg = update.effective_message
     first_name = update.effective_user.first_name
     NUMBERS = random.choice(NUM1)
-    msg.reply_video_note[MEDIA]("{} 's cock size is {}".format(
+    msg.reply_text("🍆 {} 's cock size is {}".format(
         first_name, NUMBERS
     )) 
 
+def cute(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    first_name = update.effective_user.first_name
+    NUMBERS = random.choice(NUM2)
+    msg.reply_text("🍑 {} is {} Cute!".format(
+        first_name, NUMBERS
+    )) 
+
+def boobs(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    first_name = update.effective_user.first_name
+    NUMBERS = random.choice(NUM3)
+    msg.reply_text("🍒 {} BooBs is {}!".format(
+        first_name, NUMBERS
+    )) 
+
+def lesbian(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    first_name = update.effective_user.first_name
+    NUMBERS = random.choice(NUM2)
+    msg.reply_text("💜 {} is {} lezbian!".format(
+        first_name, NUMBERS
+    )) 
+
+def horny(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    first_name = update.effective_user.first_name
+    NUMBERS = random.choice(NUM2)
+    msg.reply_text("🔥 {} is {} horny!".format(
+        first_name, NUMBERS
+    ))     
+
 
 COCK_HANDLER = DisableAbleCommandHandler("cock", cock, run_async=True)
+CUTE_HANDLER = DisableAbleCommandHandler("cute", cute, run_async=True)
+BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs, run_async=True)
+HORNY_HANDLER = DisableAbleCommandHandler("horny", horny, run_async=True)
+LESBIAN_HANDLER = DisableAbleCommandHandler("lesbian", lesbian, run_async=True)
 
 dispatcher.add_handler(COCK_HANDLER)
+dispatcher.add_handler(CUTE_HANDLER)
+dispatcher.add_handler(BOOBS_HANDLER)
+dispatcher.add_handler(HORNY_HANDLER)
+dispatcher.add_handler(LESBIAN_HANDLER)
