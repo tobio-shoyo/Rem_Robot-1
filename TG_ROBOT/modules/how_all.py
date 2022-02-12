5f8f71bf3e4307951c755182c0d86185a99515a8
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-12 11:22:53 +0530

    Create how_all.py

---
+++ TG_ROBOT/modules/how_all.py
@@ -0,0 +1,29 @@
+import html
+import random
+from TG_ROBOT import dispatcher
+from telegram import ParseMode, Update, Bot
+from TG_ROBOT.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+from telethon import Button, events
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram.utils.helpers import escape_markdown
+
+NUM1 = (
+    "6",
+    "9",
+    "15",
+    "16",
+)
+
+def cock(update: Update, context: CallbackContext):
+    args = context.args
+    first_name = update.effective_user.first_name
+    update.effective_message.reply_text("{}your dick size is"(random.choice(NUM1)))
+    escape_markdown(first_name),
+
+
+
+
+COCK_HANDLER = DisableAbleCommandHandler("cock", cock, run_async=True)
+
+dispatcher.add_handler(COCK_HANDLER)
