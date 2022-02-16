afba42c10467ce9707e8312aeb635eb0ce2b591f
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-16 23:47:24 +0530

    Update decorators.py

--- TG_ROBOT/modules/helper_funcs/decorators.py
+++ TG_ROBOT/modules/helper_funcs/decorators.py
@@ -1,179 +1,100 @@
-from TG_ROBOT.modules.disable import (
-    DisableAbleCommandHandler,
-    DisableAbleMessageHandler,
-)
-from telegram.ext import (
-    CommandHandler,
-    MessageHandler,
-    CallbackQueryHandler,
-    InlineQueryHandler,
-)
-
-from telegram.ext.filters import MessageFilter
-from TG_ROBOT import dispatcher as d, LOGGER
+from TG_ROBOT.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, InlineQueryHandler
+from telegram.ext.filters import BaseFilter
+from TG_ROBOT import dispatcher as d, log
 from typing import Optional, Union, List
 
 
-class remHandler:
-    def init(self, d):
+class KigyoTelegramHandler:
+    def __init__(self, d):
         self._dispatcher = d
 
     def command(
-        self,
-        command: str,
-        filters: Optional[MessageFilter] = None,
-        admin_ok: bool = False,
-        pass_args: bool = False,
-        pass_chat_data: bool = False,
-        run_async: bool = True,
-        can_disable: bool = True,
-        group: Optional[Union[int, str]] = 40,
+            self, command: str, filters: Optional[BaseFilter] = None, admin_ok: bool = False, pass_args: bool = False,
+            pass_chat_data: bool = False, run_async: bool = True, can_disable: bool = True,
+            group: Optional[int] = 40
     ):
+
         def _command(func):
             try:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                            admin_ok=admin_ok,
-                        ),
-                        group,
+                        DisableAbleCommandHandler(command, func, filters=filters, run_async=run_async,
+                                                  pass_args=pass_args, admin_ok=admin_ok), group
                     )
                 else:
                     self._dispatcher.add_handler(
-                        CommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                        ),
-                        group,
+                        CommandHandler(command, func, filters=filters, run_async=run_async, pass_args=pass_args), group
                     )
-                LOGGER.debug(
-                    f"[REMCMD] Loaded handler {command} for function {func.name} in group {group}"
-                )
+                log.debug(f"[KIGCMD] Loaded handler {command} for function {func.__name__} in group {group}")
             except TypeError:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                            admin_ok=admin_ok,
-                            pass_chat_data=pass_chat_data,
-                        )
+                        DisableAbleCommandHandler(command, func, filters=filters, run_async=run_async,
+                                                  pass_args=pass_args, admin_ok=admin_ok, pass_chat_data=pass_chat_data)
                     )
                 else:
                     self._dispatcher.add_handler(
-                        CommandHandler(
-                            command,
-                            func,
-                            filters=filters,
-                            run_async=run_async,
-                            pass_args=pass_args,
-                            pass_chat_data=pass_chat_data,
-                        )
+                        CommandHandler(command, func, filters=filters, run_async=run_async, pass_args=pass_args,
+                                       pass_chat_data=pass_chat_data)
                     )
-                LOGGER.debug(
-                    f"[REMCMD] Loaded handler {command} for function {func.name}"
-                )
+                log.debug(f"[KIGCMD] Loaded handler {command} for function {func.__name__}")
 
             return func
 
         return _command
 
-def message(
-        self,
-        pattern: Optional[str] = None,
-        can_disable: bool = True,
-        run_async: bool = True,
-        group: Optional[Union[int, str]] = 60,
-        friendly=None,
-    ):
+    def message(self, pattern: Optional[BaseFilter] = None, can_disable: bool = True, run_async: bool = True,
+                group: Optional[int] = 60, friendly=None):
         def _message(func):
             try:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleMessageHandler(
-                            pattern, func, friendly=friendly, run_async=run_async
-                        ),
-                        group,
+                        DisableAbleMessageHandler(pattern, func, friendly=friendly, run_async=run_async), group
                     )
                 else:
                     self._dispatcher.add_handler(
                         MessageHandler(pattern, func, run_async=run_async), group
                     )
-                LOGGER.debug(
-                    f"[REMMSG] Loaded filter pattern {pattern} for function {func.name} in group {group}"
-                )
+                log.debug(f"[KIGMSG] Loaded filter pattern {pattern} for function {func.__name__} in group {group}")
             except TypeError:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleMessageHandler(
-                            pattern, func, friendly=friendly, run_async=run_async
-                        )
+                        DisableAbleMessageHandler(pattern, func, friendly=friendly, run_async=run_async)
                     )
                 else:
                     self._dispatcher.add_handler(
                         MessageHandler(pattern, func, run_async=run_async)
                     )
-                LOGGER.debug(
-                    f"[REMMSG] Loaded filter pattern {pattern} for function {func.name}"
-                )
+                log.debug(f"[KIGMSG] Loaded filter pattern {pattern} for function {func.__name__}")
 
             return func
 
         return _message
 
-def callbackquery(self, pattern: str = None, run_async: bool = True):
+    def callbackquery(self, pattern: str = None, run_async: bool = True):
         def _callbackquery(func):
-            self._dispatcher.add_handler(
-                CallbackQueryHandler(
-                    pattern=pattern, callback=func, run_async=run_async
-                )
-            )
-            LOGGER.debug(
-                f"[REMCALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.name}"
-            )
+            self._dispatcher.add_handler(CallbackQueryHandler(pattern=pattern, callback=func, run_async=run_async))
+            log.debug(f'[KIGCALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.__name__}')
             return func
 
         return _callbackquery
 
-def inlinequery(
-        self,
-        pattern: Optional[str] = None,
-        run_async: bool = True,
-        pass_user_data: bool = True,
-        pass_chat_data: bool = True,
-        chat_types: List[str] = None,
-    ):
+    def inlinequery(self, pattern: Optional[str] = None, run_async: bool = True, pass_user_data: bool = True,
+                    pass_chat_data: bool = True, chat_types: List[str] = None):
         def _inlinequery(func):
             self._dispatcher.add_handler(
-                InlineQueryHandler(
-                    pattern=pattern,
-                    callback=func,
-                    run_async=run_async,
-                    pass_user_data=pass_user_data,
-                    pass_chat_data=pass_chat_data,
-                    chat_types=chat_types,
-                )
-            )
-            LOGGER.debug(
-                f"[REMINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.name} | PASSES USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}"
-            )
+                InlineQueryHandler(pattern=pattern, callback=func, run_async=run_async, pass_user_data=pass_user_data,
+                                   pass_chat_data=pass_chat_data, chat_types=chat_types))
+            log.debug(
+                f'[KIGINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES '
+                f'USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}')
             return func
 
         return _inlinequery
 
 
-remcmd = remHandler(d).command
-remmsg = remHandler(d).message
-remcallback = remHandler(d).callbackquery
-reminline = remHandler(d).inlinequery
+remcmd = KigyoTelegramHandler(d).command
+remmsg = KigyoTelegramHandler(d).message
+remcallback = KigyoTelegramHandler(d).callbackquery
+reminline = KigyoTelegramHandler(d).inlinequery
