d401ae10ad0fda1714fb2b22bb778c0a17782d68
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-16 22:50:52 +0530

    Update antichannel.py

--- TG_ROBOT/modules/antichannel.py
+++ TG_ROBOT/modules/antichannel.py
@@ -1,13 +1,14 @@
-from telegram.ext.filters import Filters
-from TG_ROBOT.modules.helper_funcs.decorators import kigcmd, kigmsg
-from telegram import Update, message
-from telegram.ext import CallbackContext
-from TG_ROBOT.modules.helper_funcs.anonymous import user_admin, AdminPerms
 import html
+
+from telegram.ext.filters import Filters
+from telegram import Update, message, ParseMode
+from telegram.ext import CallbackContext
+
+from TG_ROBOT.modules.helper_funcs.decorators import siestacmd, siestamsg
+from TG_ROBOT.modules.helper_funcs.channel_mode import user_admin, AdminPerms
 from TG_ROBOT.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 
-
-@kigcmd(command="antichannel", group=100)
+@siestacmd(command="antichannelmode", group=100)
 @user_admin(AdminPerms.CAN_RESTRICT_MEMBERS)
 def set_antichannel(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -27,8 +28,7 @@ def set_antichannel(update: Update, context: CallbackContext):
     message.reply_html(
         "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
-
-@kigmsg(Filters.chat_type.groups, group=110)
+@siestamsg(Filters.chat_type.groups, group=110)
 def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
@@ -39,3 +39,19 @@ def eliminate_channel(update: Update, context: CallbackContext):
         message.delete()
         sender_chat = message.sender_chat
         bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
+        
+__help__ = """
+──「 Anti-Channels 」──
+
+    ⚠️ WARNING ⚠️
+
+*IF YOU USE THIS MODE, THE RESULT IS IN THE GROUP FOREVER YOU CAN'T CHAT USING THE CHANNEL*
+
+Anti Channel Mode is a mode to automatically ban users who chat using Channels. 
+This command can only be used by *Admins*.
+
+❂ /antichannelmode <'on'/'yes'> *:* enables anti-channel-mode
+❂ /antichannelmode <'off'/'no'> *:* disabled anti-channel-mode
+"""
+
+__mod_name__ = "Anti-Channel"
