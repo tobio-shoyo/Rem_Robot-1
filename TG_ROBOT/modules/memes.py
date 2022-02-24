ef25c85deb83e0d3fd4cdedcbc1f7e07573a94ca
ANKUSH SAHOO <98841548+T-O-B-I-I@users.noreply.github.com>
2022-02-24 19:15:28 +0530

    Update memes.py

--- TG_ROBOT/modules/memes.py
+++ TG_ROBOT/modules/memes.py
@@ -1,3 +1,30 @@
+"""
+MIT License
+
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2021 Awesome-RJ
+Copyright (c) 2021, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
+
+This file is part of @TG_ROBOT (Telegram Bot)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import requests
 import asyncio
@@ -110,7 +137,7 @@ async def chika(event):
 @register(pattern="^/truth ?(.*)")
 async def _(td):
     try:
-        resp = requests.get(random.choice(fun_strings.TRUTH_STRINGS)).json()
+        resp = random.choice(fun_strings.TRUTH_STRINGS).json()
         results = f"{resp['message']}"
         return await td.reply(results)
     except Exception:
@@ -120,7 +147,7 @@ async def _(td):
 @register(pattern="^/dare ?(.*)")
 async def _(dr):
     try:
-        resp = requests.get(random.choice(fun_strings.DARE_STRINGS)).json()
+        resp = random.choice(fun_strings.DARE_STRINGS).json()
         results = f"{resp['message']}"
         return await dr.reply(results)
     except Exception:
