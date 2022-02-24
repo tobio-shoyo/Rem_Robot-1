169cc2a3222ad2115e0f2d25f932154f59d2eaab
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-24 16:31:28 +0530

    Update __main__.py

--- TG_ROBOT/__main__.py
+++ TG_ROBOT/__main__.py
@@ -351,10 +351,7 @@ async def forceclose(_, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     query, user_id = callback_request.split("|")
-    if CallbackQuery.from_user.id != int(user_id):
-        return await CallbackQuery.answer(
-            "You're not allowed to close this.", show_alert=True
-        )
+    
     await CallbackQuery.message.delete()
     await CallbackQuery.answer()    
 
